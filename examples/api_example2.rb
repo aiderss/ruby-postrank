f95cbe604ef8395fa26dae5def484bb032870afc
dan sinclair <dj2@everburning.com>
2008-11-09 14:15:31 +0800

    update to new api
    Signed-off-by: AideRSS <ilya+github@aiderss.com>

--- examples/api_example2.rb
+++ examples/api_example2.rb
@@ -5,33 +5,34 @@ require 'postrank'
 
 include PostRank
 
-s = Server.new "com.everburning"
+conn = Connection.instance
+conn.appkey = "everburning.com/ruby-postrank"
 
 feedUrl = "http://feeds.feedburner.com/spaetzel"
 puts "Running PostRank tests on #{feedUrl}"
 
-f = s.feed feedUrl
-puts "Feed Id is #{f.feed_id}"
+f = Feed.find_by_url(feedUrl)
+puts "Feed Id is #{f.id}"
 
 entries = f.entries
-puts "The PostRank of '#{entries[5].title}' is '#{entries[5].post_rank}'"
+puts "The PostRank of '#{entries[5].title}' is '#{entries[5].postrank}'"
 
-entry = f.top_posts(:period => Period::MONTH, :count => 1).first
-puts "The top post in the last month is '#{entry.title}' with a PostRank of '#{entry.post_rank}'"
+entry = f.topposts(:period => Period::YEAR, :count => 1).first
+puts "The top post in the last year is '#{entry.title}' with a PostRank of '#{entry.postrank}'"
 
 urls = ["http://flickr.com/photos/14009462@N00/2654539960/",
         "http://www.flickr.com/photos/21418584@N07/2447928272/",
         "http://www.flickr.com/photos/pilou/2655293624/"]
 
-s.post_rank(urls).each do |entry|
-  puts "The Thematic PostRank of '#{entry.title}' is '#{entry.post_rank}'"
+PostRank::PostRank.calculate(urls).each do |entry|
+  puts "The Thematic PostRank of '#{entry.original_link}' is '#{entry.postrank}'"
 end
 
-f2 = s.feed("http://feeds.feedburner.com/deysca")
+f2 = Feed.find_by_url("http://feeds.feedburner.com/deysca")
 
-s.post_rank(["http://feeds.feedburner.com/~r/Deysca/~3/329747930/",
+PostRank::PostRank.calculate(["http://feeds.feedburner.com/~r/Deysca/~3/329747930/",
              "http://feeds.feedburner.com/~r/spaetzel/~3/326933691/"],
             :feeds => [f, f2]).each do |entry|
-  puts "The Feed thematic PostRank of '#{entry.title}' is '#{entry.post_rank}'"
+  puts "The Feed thematic PostRank of '#{entry.original_link}' is '#{entry.postrank}'"
 end
 
