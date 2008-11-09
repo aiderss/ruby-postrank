ad042e7de1e454f7dfbe48ecb501e6be1359b01b
dan sinclair <dj2@everburning.com>
2008-11-09 14:09:41 +0800

    updated to new api
    Signed-off-by: AideRSS <ilya+github@aiderss.com>

--- examples/simple_example.rb
+++ examples/simple_example.rb
@@ -5,31 +5,33 @@ require 'postrank'
       
 include PostRank
 
-server = Server.new("com.everburning")
-eb = server.feed("http://everburning.com")
+conn = Connection.instance
+conn.appkey = "everburning.com/ruby-postrank"
+
+eb = Feed.find_by_url("http://everburning.com")
     
 puts "The GREAT everburning feeds"
 eb.entries(:level => Level::GREAT).each do |entry|
-  puts entry 
+  puts entry.title
 end 
 
 puts "\nThe top 5 posts in the last week on everburing"
-eb.top_posts(:period => Period::WEEK).each do |entry|
+eb.topposts(:period => Period::WEEK).each do |entry|
   puts "#{entry.title} -- #{entry.post_rank} -- #{entry.post_rank_color}"
 end
 
 puts "\nGet thematic PostRanked items"
-server.post_rank(["http://flickr.com/photos/14009462@N00/2654539960/",
+PostRank::PostRank.calculate(["http://flickr.com/photos/14009462@N00/2654539960/",
                   "http://www.flickr.com/photos/21418584@N07/2447928272/",
                   "http://www.flickr.com/photos/pilou/2655293624/"]).each do |entry|
-  puts entry
+  puts "#{entry.original_link} #{entry.postrank}"
 end
 
 puts "\nGet PostRanked items"
-server.post_rank(['http://everburning.com/news/on-recent-media/',
+PostRank::PostRank.calculate(['http://everburning.com/news/on-recent-media/',
                   'http://everburning.com/news/californication/',
                   'http://everburning.com/news/the-weary-traveler/'],
             :feeds => [eb]).each do |entry|
-  puts entry
+  puts "#{entry.original_link} #{entry.postrank}"
 end
 
